5df3663605cb98386c5ca9fa196b5ea6861f581a
Beren Millidge <bmillidgework@gmail.com>
2017-04-13 17:58:54 +0100

    initial factor graph commit

---
+++ factor-graphs.py
@@ -0,0 +1,10 @@
+from __future__ import division
+import numpy as np
+
+pass
+
+#to be impleented later. This is where I do my understandign-checking implementation of various things relating to factor graphs, as wel as probably markov networks nd belief nets and soforthwhich are similar graphial models. this to implement must be the sum product algorithm ,as well as the conversion algorithms between the different types of graphical model. also stuff like the max prodcut algorith m seems likely to be uesful too imho. Also the conditional independence calculating algorithm seems tob be quite important too imho.  Next up will be things like hjunction trees. I do need to get so much better at studying, i think going to the library early with Mycah is my best bet, tomorrow I'll try to get a serious 5 hours of study in in the morning if we can be in the library- and silent by 11. we're not silent at all in FH, and really barely do any work. ugh.
+
+#so yeah, I'm not sure how I shuold represent factor graphs necessarily except as adjacency matrices - this'll work wel for BNs and MNs  but factor graphs might be more tricky, we might need a factor-variable and a variable-factor adjacency matrix rather than a singl one. I just don't know
+
+#I'm also not sure how to actually implement the directed factor graphs in such an adjacency matrix. I mean we cuold obviously do them as different values in the matrix, but I don't know. think on this, and ideally start to implement some of these soon, as it is important you actually do pmr revision unlike mlpr which you just totally failed! you're almost half way through your pmr revision and you've done bascially nothing. you're almost half way through your exam revision time and you've done bascially nothing. you need to stop rprocrastinating like a cuck and get out there!
