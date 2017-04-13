169abd837ea0116cc80540e3ef8e6905af8421f2
Beren Millidge <bmillidgework@gmail.com>
2017-04-13 17:59:32 +0100

    initial rejection sampler commit
    hopefully many in the MCMC series. We'll have at least Gibbs and Metropolis Hastings here later.

---
+++ rejection-sampling.py
@@ -0,0 +1,5 @@
+from __future__ import division
+import numpy as np
+
+pass
+#okay, here the aim is to implement your rejection sampling algorithm and tocome up with some cool examples. this really shouldn't be that difficult or that many lines of code.
