ef6d6a9e5e8e63daa61d4f354d4671df460a938f
Beren Millidge <bmillidgework@gmail.com>
2017-04-15 00:59:29 +0100

    hamiltonian and SW MCMC
    initial commits. work on these to actually implement later

---
+++ hamiltonian-MC.py
@@ -0,0 +1,7 @@
+### HAMILTONIAN MCMC DEMO ###
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+#do!
---
+++ swendson-wang-MC.py
@@ -0,0 +1,7 @@
+### SWENDSON-WANG MCMC DEMO ###
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+#do!
